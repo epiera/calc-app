959c65b4842b7e556ecdffc4838163bf77cc8aeb
Edgar Piera <edgar.piera@gmail.com>
2022-05-12 16:00:07 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+/* eslint no-eval: 0*/
+import React, {useState} from "react"
+import words from 'lodash.words'
+import Result from "./components/Result"
+import MathOperations from "./components/MathOperations"
+import Functions from "./components/Functions"
+import "./App.css"
+import Numbers from "./components/Numbers"
+
+// Función Flecha o Arrow Function
+const App = () => {
+
+	// Array Destructuring
+	// 1era posición: valor (que inicialmente es el valor por defecto)
+	// 2nda posición: función que me permite modificar el valor por defecto
+	// [xxxx], [setxxxx]
+	const [stack, setStack] = useState("")
+
+	const items = words(stack, /[^-^+^*^/]+/g)
+
+	// Lo que ejecuta la función
+	console.log("Renderización de App", items)
+
+	const value = items.length > 0 ? items[items.length-1] : "0"
+
+	return (
+		<main className="react-calculator">
+			<Result value={value}></Result>
+			<Numbers
+				onClickNumber={number => {
+					console.log("number:", number)
+					setStack(`${stack}${number}`)
+				}}>
+			</Numbers>
+			<Functions
+				onContentClear={() => {
+					console.log("clear")
+					setStack("")
+				}}
+				onDelete={() => {
+					if (stack.length > 0){
+						console.log("delete")
+						const newStack = stack.substring(0,stack.length -1)
+						setStack(newStack)
+					}
+				}}>
+			</Functions>
+			<MathOperations
+				onClickOperation={operation => {
+					console.log("operation:", operation)
+					setStack(`${stack}${operation}`)
+				}}
+				onClickEqual={equal => {
+					console.log("equal:", equal)
+					setStack(eval(stack).toString())
+				}}>
+			</MathOperations>
+		</main>
+	)
+}
+
+export default App
+
